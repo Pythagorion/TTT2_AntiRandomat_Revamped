fadb24aa5171b1c7a52944acfd40c411bb9cf53d
Pythagorion <43889730+Pythagorion@users.noreply.github.com>
2021-10-07 21:09:13 +0200

    Delete lua/terrortown/anti_-_randomat directory

--- lua/terrortown/anti_-_randomat/cl_msg.lua
+++
@@ -1,6 +0,0 @@
-net.Receive("AntiRandomatMSG", function()
-    local message_str = net.ReadString()
-
-    EPOP:AddMessage({text = message_str, color = 133, 1, 25,255}, "", 10)
-    surface.PlaySound("weapons/init_c4_cs.wav") 
-end)
